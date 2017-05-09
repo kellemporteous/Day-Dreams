4cd7ac142adf889155e535d355b7b48635ec38ba
Kellem Porteous <1005781@student.sae.edu.au>
2017-05-09 14:48:05 +1000

    Right now this repo only contains a tool to cull layers depending on which camera you are currently using (Camera Controller GameObject)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Camera sonPerspective;
+    public Camera motherPerspective;
+
+    // Use this for initialization
+    void Start ()
+    {
+        sonPerspective.enabled = true;
+        motherPerspective.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        sonPerspective.cullingMask = ~(1 << 9);
+        motherPerspective.cullingMask = ~(1 << 8);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            sonPerspective.enabled = !sonPerspective.enabled;
+            motherPerspective.enabled = !motherPerspective.enabled;
+        }
+	}
+}
