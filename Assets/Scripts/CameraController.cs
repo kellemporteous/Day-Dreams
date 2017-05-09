b58bbba93889f39ea85eb63b3a5178e07dec36c0
Kellem Porteous <1005781@student.sae.edu.au>
2017-05-09 17:02:10 +1000

    Worked on player movement and different player states

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,21 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
+    public static CameraController Instance;
 
     public Camera sonPerspective;
     public Camera motherPerspective;
+
+    public bool sonOn;
+    public bool motherOn;
+
+
+    void Awake()
+    {
+        Instance = this;
+    }
 
     // Use this for initialization
     void Start ()
     {
         sonPerspective.enabled = true;
         motherPerspective.enabled = false;
+
+        sonOn = true;
+        motherOn = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-
         sonPerspective.cullingMask = ~(1 << 9);
         motherPerspective.cullingMask = ~(1 << 8);
 
@@ -25,6 +38,17 @@ public class CameraController : MonoBehaviour {
         {
             sonPerspective.enabled = !sonPerspective.enabled;
             motherPerspective.enabled = !motherPerspective.enabled;
+
+            if (sonOn == true)
+            {
+                sonOn = false;
+                motherOn = true;
+            }
+            else if (motherOn == true)
+            {
+                sonOn = true;
+                motherOn = false;
+            }
         }
 	}
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody sonRB;
+    private Rigidbody motherRB;
+
+    public enum MotherState
+    {
+        motherIdle,
+        motherActive
+    }
+
+    public enum SonState
+    {
+        sonIdle,
+        sonActive
+    }
+
+    public MotherState motherState;
+    public SonState sonState;
+
+	// Use this for initialization
+	void Start ()
+    {
+        sonRB = GameObject.FindGameObjectWithTag("Son").GetComponent<Rigidbody>();
+        motherRB = GameObject.FindGameObjectWithTag("Mother").GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        AILogic();
+        AIBehaviour();
+    }
+
+    void AILogic()
+    {
+        if (CameraController.Instance.sonOn == true)
+        {
+            sonState = SonState.sonActive;
+            motherState = MotherState.motherIdle;
+        }
+        if (CameraController.Instance.motherOn == true)
+        {
+            sonState = SonState.sonIdle;
+            motherState = MotherState.motherActive;
+        }
+    }
+
+    void AIBehaviour()
+    {
+        switch (sonState)
+        {
+            case SonState.sonActive:
+                Movement();
+                break;
+            case SonState.sonIdle:
+                Idle();
+                break;
+        }
+
+        switch (motherState)
+        {
+            case MotherState.motherActive:
+                Movement();
+                break;
+            case MotherState.motherIdle:
+                Idle();
+                break;
+        }
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (CameraController.Instance.sonOn == true)
+            {
+                sonRB.AddForce(Vector3.right * speed * Time.deltaTime);
+            }
+            else if (CameraController.Instance.motherOn == true)
+            {
+                motherRB.AddForce(Vector3.right * speed * Time.deltaTime);
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (CameraController.Instance.sonOn == true)
+            {
+                sonRB.AddForce(Vector3.left * speed * Time.deltaTime);
+            }
+            else if (CameraController.Instance.motherOn == true)
+            {
+                motherRB.AddForce(Vector3.left * speed * Time.deltaTime);
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (CameraController.Instance.sonOn == true)
+            {
+                sonRB.AddForce(Vector3.forward * speed * Time.deltaTime);
+            }
+            else if (CameraController.Instance.motherOn == true)
+            {
+                motherRB.AddForce(Vector3.forward * speed * Time.deltaTime);
+            }
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (CameraController.Instance.sonOn == true)
+            {
+                sonRB.AddForce(Vector3.back * speed * Time.deltaTime);
+            }
+            else if (CameraController.Instance.motherOn == true)
+            {
+                motherRB.AddForce(Vector3.back * speed * Time.deltaTime);
+            }
+        }
+    }
+
+    void Idle()
+    {
+        if (CameraController.Instance.sonOn == true)
+        {
+            motherRB.isKinematic = true;
+            sonRB.isKinematic = false;
+        }
+        if (CameraController.Instance.motherOn == true)
+        {
+            sonRB.isKinematic = true;
+            motherRB.isKinematic = false;
+        }
+    }
+}
